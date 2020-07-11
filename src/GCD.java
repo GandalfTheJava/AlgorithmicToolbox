6ce8f78d2b1bb10387708401e0700b88ff0dccff
mehmet <mehmetcimen23@gmail.com>
2020-07-11 18:00:52 +0100

    updates

--- src/GCD.java
+++ src/GCD.java
@@ -1,9 +1,16 @@
 
+
 public class GCD {
 
 	public static void main(String[] args) {
-		//GCD of large numbers
+		
+		System.out.println(Euc(357,234));
 
 	}
 
+	private static int Euc(int a,int b) {
+		if(b == 0) return a;
+		//
+		return Euc(b, a%b);
+	}
 }
